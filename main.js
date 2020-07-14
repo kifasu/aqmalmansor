991bf02b0d9aca6145eab9abe3c5aecbf97e63b6
kifasu <50930584+kifasu@users.noreply.github.com>
2020-07-14 16:35:19 +0800

    Updated

--- main.js
+++ main.js
@@ -15,3 +15,17 @@ selectElement(".icon").addEventListener("click", () => {
         }
     })
 });
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        selectElement(".nav-list").classList.toggle("active");
+        selectElement(".icon").classList.toggle("toggle");
+        navLinks.forEach((link, index) => {
+            if (link.style.animation){
+                link.style.animation = ""
+            }else{
+                link.style.animation = `navLinkAnimate 0.5s ease forwards ${ index/7 + 0.5}s`
+                console.log(index/7 + 0.5)
+            }
+        })
+    })
+})
